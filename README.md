685efc10679bd3a6126f7fa2eddbe3b7968e72da
KhoirulUmmam <rezpectae@gmail.com>
2023-01-29 20:36:25 +0700

    fetching Paths for getStaticPaths

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Hello guys, i'am a beginner to [Learn Next.js](https://nextjs.org/learn)
+
+* getSaticPaths:
+- have an Error: getStaticPaths is required for dynamic SSG pages and is missing for '/posts/[postId]'
+- when we use getStaticPaths, then the `fallback` key must be returned from getStaticPaths
--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,15 +1,53 @@
 import React from 'react'
 
-export async function getStaticProps(context){
-    const { params } = context
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    const data = res.json()
+export async function getStaticProps(context) {
+  const { params } = context
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  )
+  const data = await response.json()
 
-    return {
-        props: {
-            post: data.slice(0, 3)
-        }
+  return {
+    props: {
+      post: data
     }
+  }
+}
+
+export async function getStaticPaths(){
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await res.json()
+
+  const paths = data.map((post) => {
+    return{
+      params: {
+        postId: `${post.id}`,
+      },
+    }
+  })
+
+  return {
+    // paths: [
+    //   {
+    //     params: { postId: '1' },  
+    //   },
+    //   {
+    //     params: { postId: '2' },
+    //   },
+    //   {
+    //     params: { postId: '3' },
+    //   },
+    //   {
+    //     params: { postId: '4' },
+    //   },
+    //   {
+    //     params: { postId: '5' },
+    //   },
+    // ],
+    paths,
+    fallback: false,
+  }
 }
 
 export default function Post({ post }) {
